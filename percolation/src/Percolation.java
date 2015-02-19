176d77d132ea3b6a35716f13a2006443811b52d2
Dmitry Plechystyy <d.plechystyy@ism-ukraine.com>
2015-02-19 16:36:39 +0200

    Completely rewrote the algorithm. Now solution is without virtual vertices. All functional, memory & timing tests passed correctly. Also ignored bin folder

--- percolation/src/Percolation.java
+++ percolation/src/Percolation.java
@@ -1,35 +1,94 @@
 public class Percolation {
     private static final byte SITE_BLOCKED = 0;
     private static final byte SITE_OPEN = 1;
+    private static final byte SITE_CONNECTED_TO_TOP = 2;
+    private static final byte SITE_CONNECTED_TO_BOTTOM = 4;
 
-    private int[][] grid;
+    private byte[][] grid;
 
     private int N;
 
     private WeightedQuickUnionUF uf;
     private int ufSize;
+    
+    private boolean percolates;
 
     public Percolation(int N) {
         if (N <= 0)
             throw new IllegalArgumentException();
         
         this.N = N;
-        grid = new int[N][N];
+        grid = new byte[N][N];
         for (int i = 0; i < N; i++)
             for (int j = 0; j < N; j++)
                 grid[i][j] = SITE_BLOCKED;
 
-        // initialized WeightedQuickUnionUF with N^2+2 cells. top cell to link
-        // the first N cells, and bottom cell to link last N cells
-        ufSize = N * N + 2;
+        ufSize = N * N;
         uf = new WeightedQuickUnionUF(ufSize);
 
         for (int i = 0; i < N; i++) {
-            uf.union(0, i + 1);
-            uf.union(ufSize - 1, ufSize - 1 - 1 - i);
+            grid_connectSiteToTop(0, i); // connect all bottom cells to the bottom
+            grid_connectRootToBottom(N-1, i); // connect all bottom cells to the bottom
         }
+        
+        grid_connectSiteToTop(0, 0); // need to connect only 1st cell in the grid to top, because the whole first row is connected
+        
+        percolates = false;
     }
-
+    
+    private Tuple<Integer, Integer> grid_getRoot(int i, int j) {
+        int root = uf.find(xyTo1D(i, j));
+        
+        int rootI, rootJ;
+        rootI = root / N;
+        rootJ = root % N;
+        return new Tuple<Integer, Integer>(rootI, rootJ);
+    }
+    
+    private void grid_connectRootToTop(int rootI, int rootJ) {
+        grid[rootI][rootJ] |= SITE_CONNECTED_TO_TOP;
+    }
+    
+    private void grid_connectSiteToTop(int i, int j) {
+        Tuple<Integer, Integer> root = grid_getRoot(i, j);
+        grid_connectRootToTop(root.x, root.y);
+    }
+    
+    private void grid_connectRootToBottom(int rootI, int rootJ) {
+        grid[rootI][rootJ] |= SITE_CONNECTED_TO_BOTTOM;
+    }
+    
+    private void grid_connectSiteToBottom(int i, int j) {
+        Tuple<Integer, Integer> root = grid_getRoot(i, j);
+        grid_connectRootToBottom(root.x, root.y);
+    }
+    
+    private void grid_openSite(int i, int j) {
+        grid[i][j] |= SITE_OPEN;
+    }
+    
+    private boolean grid_isRootConnectedToTop(int rootI, int rootJ) {
+        return (grid[rootI][rootJ] & SITE_CONNECTED_TO_TOP) == SITE_CONNECTED_TO_TOP;
+    }
+    
+    private boolean grid_isSiteConnectedToTop(int i, int j) {
+        Tuple<Integer, Integer> root = grid_getRoot(i, j);
+        return grid_isRootConnectedToTop(root.x, root.y);
+    }
+    
+    private boolean grid_isRootConnectedToBottom(int rootI, int rootJ) {
+        return (grid[rootI][rootJ] & SITE_CONNECTED_TO_BOTTOM) == SITE_CONNECTED_TO_BOTTOM;
+    }
+    
+    private boolean grid_isSiteConnectedToBottom(int i, int j) {
+        Tuple<Integer, Integer> root = grid_getRoot(i, j);
+        return grid_isRootConnectedToBottom(root.x, root.y);
+    }    
+    
+    private boolean grid_isSiteOpen(int i, int j) {
+        return (grid[i][j] & SITE_OPEN) == SITE_OPEN;
+    }
+    
     private void checkBounds(String methodName, int i, int j) {
         if ((i < 0) || (i >= N))
             throw new IndexOutOfBoundsException(
@@ -46,56 +105,100 @@ public class Percolation {
         int i = inputI - 1;
         int j = inputJ - 1;
         checkBounds("open", i, j);
-        grid[i][j] = SITE_OPEN;
+        if (grid_isSiteOpen(i, j)) return; //do nothing if site is already open
+        
+        grid_openSite(i, j);
         int p, q;
-
+        boolean connect2top, connect2bottom;
+        
         p = xyTo1D(i, j);
+        Tuple<Integer, Integer> rootP, rootQ;
+        rootP = grid_getRoot(i, j);
+        
+        connect2top = connect2bottom = false;
         if (j > 0) {
-            q = xyTo1D(i, j - 1);
-            if (grid[i][j - 1] == SITE_OPEN && !uf.connected(p, q)) {
+            if (grid_isSiteOpen(i, j-1)) {
+                q = xyTo1D(i, j - 1);
+                rootQ = grid_getRoot(i, j-1);
+                if (grid_isRootConnectedToTop(rootQ.x, rootQ.y))
+                    connect2top = true;
+                if (grid_isRootConnectedToBottom(rootQ.x, rootQ.y))
+                    connect2bottom = true;
                 uf.union(p, q);
             }
         }
 
         if (j < N - 1) {
-            q = xyTo1D(i, j + 1);
-            if (grid[i][j + 1] == SITE_OPEN && !uf.connected(p, q)) {
+            if (grid_isSiteOpen(i, j+1)) {
+                q = xyTo1D(i, j + 1);
+                rootQ = grid_getRoot(i, j+1);
+                if (grid_isRootConnectedToTop(rootQ.x, rootQ.y))
+                    connect2top = true;
+                if (grid_isRootConnectedToBottom(rootQ.x, rootQ.y))
+                    connect2bottom = true;
                 uf.union(p, q);
             }
         }
 
         if (i > 0) {
-            q = xyTo1D(i - 1, j);
-            if (grid[i - 1][j] == SITE_OPEN && !uf.connected(p, q)) {
+            if (grid_isSiteOpen(i-1, j)) {
+                q = xyTo1D(i-1, j);
+                rootQ = grid_getRoot(i-1, j);                
+                if (grid_isRootConnectedToTop(rootQ.x, rootQ.y))
+                    connect2top = true;
+                if (grid_isRootConnectedToBottom(rootQ.x, rootQ.y))
+                    connect2bottom = true;
                 uf.union(p, q);
             }
         }
 
         if (i < N - 1) {
-            q = xyTo1D(i + 1, j);
-            if (grid[i + 1][j] == SITE_OPEN && !uf.connected(p, q)) {
+            if (grid_isSiteOpen(i+1, j)) {
+                q = xyTo1D(i + 1, j);
+                rootQ = grid_getRoot(i+1, j);
+                if (grid_isRootConnectedToTop(rootQ.x, rootQ.y))
+                    connect2top = true;
+                if (grid_isRootConnectedToBottom(rootQ.x, rootQ.y))
+                    connect2bottom = true;
                 uf.union(p, q);
             }
         }
+        
+        rootQ = grid_getRoot(i, j);
+        if (connect2top || grid_isRootConnectedToTop(rootP.x, rootP.y))
+            grid_connectRootToTop(rootQ.x, rootQ.y);
+        if (connect2bottom || grid_isRootConnectedToBottom(rootP.x, rootP.y))
+            grid_connectRootToBottom(rootQ.x, rootQ.y);
+        
+        if (grid_isRootConnectedToTop(rootQ.x, rootQ.y) && grid_isRootConnectedToBottom(rootQ.x, rootQ.y))
+            percolates = true;
+        
     }
 
     private int xyTo1D(int i, int j) {
-        return 1 + i * N + j;
+        return i * N + j;
     }
-
+    
     public boolean isOpen(int i, int j) {
         checkBounds("isOpen", i - 1, j - 1);
-        return grid[i - 1][j - 1] == SITE_OPEN;
+        return grid_isSiteOpen(i - 1, j - 1);
     }
 
     public boolean isFull(int i, int j) {
         checkBounds("isFull", i - 1, j - 1);
-        return isOpen(i, j) && uf.connected(0, xyTo1D(i - 1, j - 1));
+        return isOpen(i, j) && grid_isSiteConnectedToTop(i-1, j-1);
     }
 
     public boolean percolates() {
-        boolean result = uf.connected(0, ufSize - 1); 
-        if (N == 1) result &= isFull(1, 1);
-        return result;
+        return this.percolates;
+    }
+    
+    private class Tuple<X, Y> { 
+        public final X x; 
+        public final Y y; 
+        public Tuple(X x, Y y) { 
+          this.x = x; 
+          this.y = y; 
+        } 
     }
 }

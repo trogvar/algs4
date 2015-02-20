f3e3c50efa2aeaec7ed6140d4ba4764f40743c78
Dmitry Plechystyy <d.plechystyy@ism-ukraine.com>
2015-02-20 17:40:57 +0200

    added a "valuable" comment

--- general/src/FixedCapacityStackOfStrings.java
+++ general/src/FixedCapacityStackOfStrings.java
@@ -1,23 +1,24 @@
-
 public class FixedCapacityStackOfStrings
 {
     private String[] s;
     private int N = 0;
-    
+
+    //this is fixed capacity for now. planned to be adjusted later
     public FixedCapacityStackOfStrings(int capacity)
     {
         s = new String[capacity];
     }
-    
-    public boolean isEmpty() {
+
+    public boolean isEmpty()
+    {
         return N == 0;
     }
-    
+
     public void push(String item)
     {
         s[N++] = item;
     }
-    
+
     public String pop()
     {
         return s[--N];

4a497fca7923c512359cdb634a25873251adf078
only.one.trogvar@gmail.com <Trogvar@Asgard>
2015-02-22 15:42:08 +0200

    Added implementation of InsertionSort

---
+++ general/src/InsertionSort.java
@@ -0,0 +1,32 @@
+public class InsertionSort {
+
+    public static void sort(Comparable[] a) {
+        int N = a.length;
+        for (int i = 1; i < N; i++)
+        {
+            for (int j = i; j > 0; j--)
+                if (less(a[j], a[j-1]))
+                    exch(a, j-1, j);
+                else
+                    break;
+        }
+    }
+    
+    private static boolean less(Comparable a, Comparable b) {
+        return a.compareTo(b) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+
+    public static void main(String[] args) {
+        String[] strings = { "a", "k", "b", "V", "z", "A", "c", "y", "F", "W" };
+        sort(strings);
+        for (String s : strings)
+            System.out.println(s);
+    }
+
+}
